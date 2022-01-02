fd03793f2ef614dbed718dc6e731e4d78e3823c6
sanjaypodishetty <96732269+sanjaypodishetty@users.noreply.github.com>
2022-01-02 11:04:35 +0530

    Update rock_paper_scissors.py

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -1,48 +1,48 @@
 import random
 l=['rock','paper','scissor']
 number=int(input("Enter number of plays: "))
-'''my_score=0
-comp_score=0'''
-def c():
-    comp_score=0
-    my_score=0
-    for x in range(0,number,1):
-        c=random.choice(l)
-        me=input("my choice: ")
-        if c==me:
-            print("Both selected the same!!")
-        elif c=='rock' and me=='paper':
-            my_score+=1
-            print("computer's choice was rock")
-        elif c=='rock' and me=='scissor':
-            comp_score+=1
-            print("computer's choice was rock")
-        elif c=='paper' and me=='scissor':
-            comp_score+=1
-            print("computer's choice was paper")
-        elif c=='paper' and me=='rock':
-            my_score+=1
-            print("computer's choice was paper")
-        elif c=='scissor' and me=='paper':
-            comp_score+=1
-            print("computer's choice was scissor")
-        elif c=='scissor' and me=='rock':
-            my_score+=1
-            print("computer's choice was scissor")
-            print("Your score is:",my_score,"\nComputer's score is:",comp_score)
-c()
-o=c.comp_score
-p=c.my_score
+my_score=0
+comp_score=0
+#def c():
+'''comp_score=0
+my_score=0'''
+for x in range(0,number,1):
+    c=random.choice(l)
+    me=input("my choice: ")
+    if c==me:
+        print("Both selected the same!!")
+    elif c=='rock' and me=='paper':
+        my_score+=1
+        print("computer's choice was rock")
+    elif c=='rock' and me=='scissor':
+        comp_score+=1
+        print("computer's choice was rock")
+    elif c=='paper' and me=='scissor':
+        comp_score+=1
+        print("computer's choice was paper")
+    elif c=='paper' and me=='rock':
+        my_score+=1
+        print("computer's choice was paper")
+    elif c=='scissor' and me=='paper':
+        comp_score+=1
+        print("computer's choice was scissor")
+    elif c=='scissor' and me=='rock':
+        my_score+=1
+        print("computer's choice was scissor")
+        
+o=comp_score
+p=my_score
 if o>p:
-    print("Computer won!!")
+    print("COMPUTER WON!!")
 elif o<p:
-   print("YOu won!!")
+    print("YOU WON!!")
 elif o==p:
     print("It's a tie")
+print("\tYour score is:",my_score,"\n\tComputer's score is:",comp_score)
 a=input("PLay Again: (Y)es / (N)o: ")
-if a.upper=="Y":
-    c()
-elif a.upper=="N":
+if a=="Y" or a=="y":
+    print(" Then run again")
+elif a=="N" or a=="n":
     print("Thank tou see you next time!")
 else:
     print("Invalid selection! Try running again")
